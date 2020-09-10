1bc08645b4cd4da48b4abddfcbc4132ee813928d
Ivaylo <ivailomarinov43@gmail.com>
2020-09-10 17:27:11 +0300

    setup of navigation bar

--- client/src/components/Navigation/Navigation.tsx
+++ client/src/components/Navigation/Navigation.tsx
@@ -1,5 +1,24 @@
-import React from 'react';
+import React from "react";
+import logo from "../../assets/logo.png";
+import medicalClinic from "../../assets/medicalClinicLogo.png";
+import styles from "./Navigation.module.scss";
+import { Button } from "@material-ui/core";
 
-const Navigation: React.FC = () => <></>;
+const Navigation: React.FC = () => (
+  <div className={styles.container}>
+    <div>
+      <img src={logo} alt="" className={styles.logo} />
+      <img src={medicalClinic} alt="" className={styles.medicalClinic} />
+    </div>
+    <div>
+      <span className={styles.navTab}>Departments</span>
+      <span className={styles.navTab}>Doctors</span>
+      <span className={styles.navTab}>About</span>
+      <Button variant="contained" color="primary">
+        Sign in
+      </Button>
+    </div>
+  </div>
+);
 
 export default Navigation;
