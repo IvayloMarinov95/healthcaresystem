bf10d787a3b0c126f8ffb1c08dac2f4eed844ff9
Ivaylo Marinov <ivailomarinov43@gmail.com>
2020-10-21 17:27:02 +0300

    finished responsiveness

--- client/src/components/Departments/Departments.tsx
+++ client/src/components/Departments/Departments.tsx
@@ -1,16 +1,16 @@
-import React, { useEffect } from "react";
+import React, { useEffect } from 'react';
 
-import Department from "./Department/Department";
-import DepartmentInfo from "./DepartmentInfo/DepartmentInfo";
-import InformationBox from "../HomePage/components/InformationBoxes/InformationBox/InformationBox";
-import OpeningHours from "../HomePage/components/InformationBoxes/OpeningHours/OpeningHours";
-import cardiac from "../../assets/cardiacclinic.jpg";
-import gynecology from "../../assets/gynecology.jpg";
-import lab from "../../assets/lab.jpg";
-import opthal from "../../assets/opthal.jpg";
-import pediatric from "../../assets/pediatricclinic.jpg";
-import styles from "./Departments.module.scss";
-import surgery from "../../assets/outpatientsurgery.jpg";
+import cardiac from '../../assets/cardiacclinic.jpg';
+import gynecology from '../../assets/gynecology.jpg';
+import lab from '../../assets/lab.jpg';
+import opthal from '../../assets/opthal.jpg';
+import surgery from '../../assets/outpatientsurgery.jpg';
+import pediatric from '../../assets/pediatricclinic.jpg';
+import InformationBox from '../HomePage/components/InformationBoxes/InformationBox/InformationBox';
+import OpeningHours from '../HomePage/components/InformationBoxes/OpeningHours/OpeningHours';
+import Department from './Department/Department';
+import DepartmentInfo from './DepartmentInfo/DepartmentInfo';
+import styles from './Departments.module.scss';
 
 const Departments: React.FC = () => {
   useEffect(() => {
@@ -23,6 +23,14 @@ const Departments: React.FC = () => {
 
   return (
     <div className={styles.container}>
+      <div>
+        <DepartmentInfo />
+        <InformationBox
+          style={styles.openingHours}
+          service="Opening Hours"
+          description={<OpeningHours />}
+        />
+      </div>
       <div className={styles.departments}>
         <Department
           image={surgery}
@@ -49,14 +57,6 @@ const Departments: React.FC = () => {
           image={lab}
           header="Laboratory Analysis"
           text="In Pharmacy"
-        />
-      </div>
-      <div>
-        <DepartmentInfo />
-        <InformationBox
-          style={styles.openingHours}
-          service="Opening Hours"
-          description={<OpeningHours />}
         />
       </div>
     </div>
