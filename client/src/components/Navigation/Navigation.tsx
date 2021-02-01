8dfd0d7162e6895d76f362e7c1c992d9166616ca
Ivaylo Marinov <i.marinov@is-bg.net>
2021-02-01 16:04:43 +0200

    hover and active on navigation links

--- client/src/components/Navigation/Navigation.tsx
+++ client/src/components/Navigation/Navigation.tsx
@@ -1,13 +1,17 @@
 import React, { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
 
 import { Button } from "react-bootstrap";
 import Carousel from "./Carousel/Slider";
 import { Link } from "react-router-dom";
 import medicalClinic from "../../assets/medicalClinicLogo.png";
 import styles from "./Navigation.module.scss";
+import classNames from "classnames";
 
 const Navigation: React.FC = () => {
-  const [scrolled, setScrolled] = useState(false);
+  const [scrolled, setScrolled] = useState<boolean>(false);
+  const [resized, setResize] = useState<boolean>(false);
+  const location = useLocation();
 
   useEffect(() => {
     const handleScroll = () => {
@@ -23,31 +27,71 @@ const Navigation: React.FC = () => {
     };
   }, []);
 
+  useEffect(() => {
+    const handleMobile = () => {
+      if (window.innerWidth < 769) {
+        setResize(true);
+      } else {
+        setResize(false);
+      }
+    };
+    window.addEventListener("resize", handleMobile);
+    return () => {
+      window.removeEventListener("resize", handleMobile);
+    };
+  }, []);
+
   return (
     <>
       <div
         className={
-          scrolled || window.outerWidth < 769
-            ? styles.containerScroll
-            : styles.container
+          scrolled || resized ? styles.containerScroll : styles.container
         }
       >
         <Link to="/" className={styles.logoContainer}>
           <img src={medicalClinic} alt="" className={styles.medicalClinic} />
         </Link>
         <div>
-          <Link to="/" className={styles.navTab}>
+          <Link
+            to="/"
+            className={
+              location.pathname === "/"
+                ? classNames(styles.navTab, styles.active)
+                : styles.navTab
+            }
+          >
             Home
           </Link>
-          {/* <Link to="/departments" className={styles.navTab}>
+          <Link
+            to="/departments"
+            className={
+              location.pathname === "/departments"
+                ? classNames(styles.navTab, styles.active)
+                : styles.navTab
+            }
+          >
             Departments
           </Link>
-          <Link to="/doctors" className={styles.navTab}>
+          <Link
+            to="/doctors"
+            className={
+              location.pathname === "/doctors"
+                ? classNames(styles.navTab, styles.active)
+                : styles.navTab
+            }
+          >
             Doctors
           </Link>
-          <Link to="/about" className={styles.navTab}>
+          <Link
+            to="/about"
+            className={
+              location.pathname === "/about"
+                ? classNames(styles.navTab, styles.active)
+                : styles.navTab
+            }
+          >
             About
-          </Link> */}
+          </Link>
           <Button variant="dark">Sign in</Button>
         </div>
       </div>
