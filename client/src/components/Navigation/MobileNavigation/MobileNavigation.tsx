e205ad1a960775d1c25bd92c929b50ae98b962fb
Ivaylo Marinov <i.marinov@is-bg.net>
2021-02-10 12:16:55 +0200

    add sign in modal to mobile nav

--- client/src/components/Navigation/MobileNavigation/MobileNavigation.tsx
+++ client/src/components/Navigation/MobileNavigation/MobileNavigation.tsx
@@ -3,10 +3,14 @@ import { Link } from "react-router-dom";
 import medicalClinic from "../../../assets/medicalClinicLogo.png";
 import styles from "../Navigation.module.scss";
 import Hamburger from "hamburger-react";
-import { Button } from "react-bootstrap";
+import { Button, Modal } from "react-bootstrap";
+import SignIn from "../SignIn/SignIn";
 
 const MobileNavigation = () => {
   const [isOpen, setOpen] = useState<boolean>(false);
+  const [openModal, setOpenModal] = useState<boolean>(false);
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
   const ref = useRef<HTMLDivElement>(null);
 
   const handleClickOutside = (event: MouseEvent) => {
@@ -21,6 +25,14 @@ const MobileNavigation = () => {
       document.removeEventListener("click", handleClickOutside, true);
     };
   }, []);
+
+  const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.target.value);
+  };
 
   return (
     <div ref={ref}>
@@ -49,11 +61,37 @@ const MobileNavigation = () => {
           <Link to="/about" className={styles.mobileTabs}>
             About
           </Link>
-          <Button variant="dark" size="lg" className={styles.mobileBtn}>
+          <Button variant="dark" size="lg" className={styles.mobileBtn}  onClick={() => setOpenModal(true)}>
             Sign in
           </Button>
         </div>
       )}
+      <Modal
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+        show={openModal}
+        onHide={() => setOpenModal(false)}
+        onEscapeKeyDown={() => setOpenModal(false)}
+        backdrop="static"
+      >
+        <Modal.Header closeButton>
+          <Modal.Title id="contained-modal-title-vcenter">Sign in</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <SignIn
+            email={email}
+            password={password}
+            handleEmailChange={handleEmailChange}
+            handlePasswordChange={handlePasswordChange}
+          />
+        </Modal.Body>
+        <Modal.Footer>
+          <Button onClick={() => console.log()}>Sign in</Button>
+          <Button variant="danger" onClick={() => setOpenModal(false)}>
+            Cancel
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </div>
   );
 };
