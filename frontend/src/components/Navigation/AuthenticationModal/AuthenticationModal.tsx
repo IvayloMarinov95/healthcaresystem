3fcc2e342de518d6cdf4c92731ca623778d80edb
Ivaylo Marinov <ivaylo.marinov@4finance.com>
2023-08-31 14:45:59 +0300

    add authorization middleware

--- frontend/src/components/Navigation/AuthenticationModal/AuthenticationModal.tsx
+++ frontend/src/components/Navigation/AuthenticationModal/AuthenticationModal.tsx
@@ -1,7 +1,10 @@
-import React from "react";
-import { Button, Modal } from "react-bootstrap";
-import SignIn from "../SignIn/SignIn";
-import SignUp from "../SignUp/SignUp";
+import React from 'react';
+import { Button, Modal } from 'react-bootstrap';
+import { useAppSelector } from '../../../app/hooks';
+import { RootState } from '../../../app/store';
+import SignIn from '../SignIn/SignIn';
+import SignUp from '../SignUp/SignUp';
+import axios from 'axios';
 
 interface Props {
   openModal: boolean;
@@ -27,53 +30,88 @@ const AuthenticationModal: React.FC<Props> = ({
   handleEmailChange,
   handlePasswordChange,
   handleModalContentChange,
-}) => (
-  <>
-    <Modal
-      aria-labelledby="contained-modal-title-vcenter"
-      centered
-      show={openModal}
-      onHide={handleHide}
-      onEscapeKeyDown={handleHide}
-      backdrop="static"
-    >
-      <Modal.Header closeButton>
-        <Modal.Title id="contained-modal-title-vcenter">
-          {toggle === "signIn" ? "Sign In" : "Sign Up"}
-        </Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-        {toggle === "signIn" && (
-          <SignIn
-            email={email}
-            password={password}
-            handleModalContentChange={() => handleModalContentChange("signUp")}
-            handleEmailChange={handleEmailChange}
-            handlePasswordChange={handlePasswordChange}
-          />
-        )}
-        {toggle === "signUp" && (
-          <SignUp
-            username={username}
-            email={email}
-            password={password}
-            handleModalContentChange={() => handleModalContentChange("signIn")}
-            handleUsernameChange={handleUsernameChange}
-            handleEmailChange={handleEmailChange}
-            handlePasswordChange={handlePasswordChange}
-          />
-        )}
-      </Modal.Body>
-      <Modal.Footer>
-        <Button onClick={() => console.log()}>
-          {toggle === "signIn" ? "Sign In" : "Sign Up"}
-        </Button>
-        <Button variant="danger" onClick={handleHide}>
-          Cancel
-        </Button>
-      </Modal.Footer>
-    </Modal>
-  </>
-);
+}) => {
+  const roles = useAppSelector((state: RootState) => state.roles.value);
+
+  const handleClick = (toggle: string) => {
+    if (toggle === 'signIn') {
+      signIn();
+    }
+    if (toggle === 'signUp') {
+      signUp();
+    }
+  };
+
+  const signIn = () => console.log();
+
+  const signUp = async () => {
+    const url = 'http://localhost:5000/api/users/signup';
+    const signUpData = {
+      name: username,
+      email,
+      password,
+      //@ts-ignore
+      role: roles.filter((item) => item.role === 'patient')[0]?._id,
+    };
+
+    await axios
+      .post(url, signUpData)
+      .then((response) => console.log(response.data))
+      .catch((error) => console.log('error: ', error));
+  };
+
+  return (
+    <>
+      <Modal
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+        show={openModal}
+        onHide={handleHide}
+        onEscapeKeyDown={handleHide}
+        backdrop="static"
+      >
+        <Modal.Header closeButton>
+          <Modal.Title id="contained-modal-title-vcenter">
+            {toggle === 'signIn' ? 'Sign In' : 'Sign Up'}
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          {toggle === 'signIn' && (
+            <SignIn
+              email={email}
+              password={password}
+              handleModalContentChange={() =>
+                handleModalContentChange('signUp')
+              }
+              handleEmailChange={handleEmailChange}
+              handlePasswordChange={handlePasswordChange}
+            />
+          )}
+          {toggle === 'signUp' && (
+            <SignUp
+              username={username}
+              email={email}
+              password={password}
+              handleModalContentChange={() =>
+                handleModalContentChange('signIn')
+              }
+              handleUsernameChange={handleUsernameChange}
+              handleEmailChange={handleEmailChange}
+              handlePasswordChange={handlePasswordChange}
+            />
+          )}
+        </Modal.Body>
+        <Modal.Footer>
+          <Button onClick={() => handleClick(toggle)}>
+            {toggle === 'signIn' ? 'Sign In' : 'Sign Up'}
+          </Button>
+          <Button variant="danger" onClick={handleHide}>
+            Cancel
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+};
 
 export default AuthenticationModal;
